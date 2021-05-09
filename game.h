d2c6090298654faef21144c85b7d61f0a7f34a06
rsl_cs <lohanravindra@gmail.com>
2021-05-09 17:16:49 +0000

    Update game.h

--- game.h
+++ game.h
@@ -11,6 +11,9 @@ void maze_setup(bool** maze);
 //Frees media and shuts down SDL
 void close();
 
-bool xcheck(int x, int y1, int y2);
-bool ycheck(int x1, int x2, int y);
-bool ccheck(int x1, int y1, int x2, int y2, int x);
+bool xcheck(int x, int y1, int y2, bool** maze);
+bool ycheck(int x1, int x2, int y, bool** maze);
+bool ccheck(int x1, int y1, int x2, int y2, int x, bool** maze);
+
+void bwin();
+void ywin();
